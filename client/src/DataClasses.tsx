604b238c255218432d24913bfabf276e0043e72f
Lucas Mayall <lucasmmayall@gmail.com>
2023-05-21 17:58:38 -0700

    Added User dataclass, will add more later

---
+++ client/src/DataClasses.tsx
@@ -0,0 +1,12 @@
+
+// User class
+export class User {
+    name: string;
+    email: string;
+    // accessToken: string;
+    // refreshToken: string;
+    constructor(name: string, email: string) {
+        this.name = name;
+        this.email = email;
+    }
+}
